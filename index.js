6d737847253b776fd02942457e78663b794f46e8
gan <wenbing_1992@hotmail.com>
2022-08-06 09:38:55 +0800

    done

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const containerWidth = 300;
 const containerHeight = 500;
 const blockArr =[];
 let blockIndex = 0;
-let blockColumn = 6;
-let blockRow = 3;
-let colWidth = containerWidth/blockColumn;
-let padding = colWidth/4;
-
-
 
 class Block{
     constructor(xAxis, yAxis, width, height){
@@ -20,21 +14,26 @@ class Block{
     }
 }
 
-const blockProp = {
-    w: colWidth,
-    h: padding,
-}
+const block = (num1,num2) => {
+    let blockColumn = num1;
+    let blockRow = num2;
+    let colWidth = containerWidth/blockColumn;
+    let padding = colWidth/4;
 
-const block = () => {
+    const blockProp = {
+        w: colWidth,
+        h: padding,
+    }
+
     for (let i = 0; i < blockColumn; i++){
         for(let j = 0; j < blockRow; j++){
-            blockArr[blockIndex] = new Block(padding/2+(i)*colWidth-colWidth/40,(colWidth*7)+j*(colWidth-2*padding),blockProp.w-padding,blockProp.h)
+            blockArr[blockIndex] = new Block(padding/2+(i)*colWidth-colWidth/40,(colWidth*6.5)+j*(colWidth-2*padding),blockProp.w-padding,blockProp.h)
             blockIndex++; 
             $block = $('<div>');
             $block.addClass('block');
             $block.css('height', blockProp.h +'px').css('width', blockProp.w-padding + 'px').css('background-color','blue').css('position','absolute');
             $('.container').append($block);
-            $block.css('left', padding/2+(i)*(colWidth)-colWidth/40 + 'px').css('bottom', (colWidth*7)+j*(colWidth-2*padding) + 'px')
+            $block.css('left', padding/2+(i)*(colWidth)-colWidth/40 + 'px').css('bottom', (colWidth*6.5)+j*(colWidth-2*padding) + 'px')
         }
     }
 }
@@ -65,13 +64,13 @@ const playerMove = () =>{
             if(currentPosition[0] > 0){
                 currentPosition[0] -= 10;
                 playerPosition();
-                ballMove();
+                //ballMove();
             }
         }if(e.keyCode === 39){
             if(currentPosition[0]< containerWidth-playerWidth){
                 currentPosition[0] += 10;
                 playerPosition();
-                ballMove();
+                //ballMove();
             }
         }
     })
@@ -100,11 +99,11 @@ const ballPosition = () =>{
 // const isRunning = true;
 // Move the ball
 const ballMove = () =>{ 
-    currentBall[0] += ballDirection[0];
+        currentBall[0] += ballDirection[0];
     currentBall[1] += ballDirection[1];
     ballPosition();
     checkforCollision();
-}
+     }
 
 
 //let timer = setInterval(ballMove,10);
@@ -115,17 +114,20 @@ const checkforCollision = () =>{
             (currentBall[0] > blockArr[i].xAxis) && (currentBall[0] < (blockArr[i].xAxis + blockArr[i].width)) && 
             (currentBall[1] > blockArr[i].yAxis) && (currentBall[1] < (blockArr[i].yAxis + blockArr[i].height))
             ){
-            console.log(currentBall)
-            console.log(i)
+            console.log(currentBall[0], currentBall[1])
+            //console.log(currentBall)
+            //console.log(i)
             Blockscore++;
             $('.score').text('Score: '+ Blockscore)
             $('.block:eq('+ i +')').remove();
             //changeblockdirection();
             changeDirection();
             blockArr.splice(i,1);
-            console.log(blockArr)
+            //console.log(blockArr)
             if(blockArr.length === 0){
                 alert('win');
+                location.reload();
+               
             }
         } 
         
@@ -148,12 +150,16 @@ const checkforCollision = () =>{
         }
      
     // check for gameover
-    if(currentBall[1]<= 10){
-        clearInterval(timerId);
+    if(currentBall[1] <= 10){
+        //console.log(timerId)
+        clearInterval(timerId1);
+        clearInterval(timerId2)
+        //console.log(speed, currentBall[1])
         lifePoint--;
         $('.Life').text('Life: '+ lifePoint);
         if(lifePoint === 0){
-            alert('Lose')
+            alert('Lose');
+            location.reload();
         }
         retryButton();
         retryclick();
@@ -161,7 +167,8 @@ const checkforCollision = () =>{
     }
 }
 
-let timerId = setInterval(ballMove,20);
+//let timerId;
+//let timerId = setInterval(ballMove,speed);
 let ballDirection = [3,2]
 
 const changeDirection = () =>{
@@ -192,22 +199,11 @@ const changeblockdirection = () =>{
     }   
 }
 
-const score = () =>{
-    const $score = $('<div>');
-    $score.addClass('score').text('Score: '+ Blockscore).css('color','white').css('font-size','20px')
-    $('.container').append($score);
-}
-
-const threeLife = () =>{
-    const $threeLife = $('<div>');
-    $threeLife.addClass('Life').text('Life: '+ 3).css('color','white').css('font-size','20px');
-    $('.container').append($threeLife);
-}
 
 const retryButton = () =>{
     const $retry = $('<button>');
     $('.container').append($retry);
-    $retry.addClass('retry').text('Continue');
+    $retry.addClass('retry').text('REPLAY');
     $($retry).css('position','absolute').css('left',(containerWidth/2-25) +'px').css('bottom',(containerHeight/2) +'px');
 }
 
@@ -228,8 +224,9 @@ const ballMove1 = () =>{
     checkforCollision();
 }
 
+let timerId2
 const resetGame = () =>{
-    timerId = setInterval(ballMove,20);
+    timerId2 = setInterval(ballMove,speed);
     ballMove1()
     $('.retry').remove();
 }
@@ -238,8 +235,14 @@ const resetGame = () =>{
 const test = () =>{
     const $container = $('<div>');
     $container.addClass('container')
-    $container.css('width', containerWidth +'px').css('height', containerHeight+ 'px').css('border', '2px solid white').css('position','absolute').css('top','10%').css('left','35%')
-    // $('body').css('background-image','url("+ https://media.npr.org/assets/img/2022/05/27/Sagittarius_A_black_hole_swarm_vert-05b6e17e76931e0c5bf153034bbfca1867d650e4.jpg + ")').css('top','0px').css('left','0px').css('background-size', 'cover')
+    $container.css('width', containerWidth +'px').css('height', containerHeight+ 'px').css('border', '2px solid white').css('position','absolute').css('top','10%').css('left','35%');
+    const $score = $('<div>');
+    $score.addClass('score')
+    $score.text('Score: '+ Blockscore).css('color','white').css('font-size','20px');
+    $container.append($score);
+    const $threeLife = $('<div>');
+    $threeLife.addClass('Life').text('Life: '+ 3).css('color','white').css('font-size','20px');
+    $container.append($threeLife);
     const $breakout = $('<div>');
     $breakout.addClass('breakout').text('BreakOut').css('color','white').css('font-size','30px');
     $('body').append($breakout);
@@ -257,126 +260,109 @@ const test = () =>{
     $startButton.css('width','100px').css('height','40px').css('border','1px solid white').css('background-color','red');
     $startButton.text('Start').css('color','white').css('position','absolute').css('font-size','20px');
     $startButton.css('left',containerWidth/11 +'px').css('bottom', '10px').css('text-align','center');
-    clearInterval(timerId);
-    $('.startButton').on('click',(() =>{
-        timerId = setInterval(ballMove,20);
-    }))
-    const $rule = $('<button>');
-    $('.testPlay').append($rule);
-    $rule.addClass('rule');
-    $rule.css('width','100px').css('height','20px').css('border','1px solid white').css('background-color','white');
-    $rule.text('Rule').css('color','black').css('position','absolute').css('font-size','15px');
-    $rule.css('left',containerWidth/11 +'px').css('bottom', '70px').css('text-align','center');
-    gameRule();
-    $('.rule').on('click',(()=>{
-        $('.desciption').show('slow');
-        $('.rule').on('click',(()=>{
-            $('.desciption').hide();
-        }))
-    }))
+    // const $rule = $('<button>');
+    // $('.testPlay').append($rule);
+    // $rule.addClass('rule');
+    // $rule.css('width','100px').css('height','20px').css('border','1px solid white').css('background-color','white');
+    // $rule.text('Rule').css('color','black').css('position','absolute').css('font-size','15px');
+    // $rule.css('left',containerWidth/11 +'px').css('bottom', '70px').css('text-align','center');
+    // // gameRule();
+    // $('.rule').on('click',(()=>{
+    //     $('.level').hide();
+    //     $('.desciption').show();
+    //     $('.rule').css('border','3px solid blue');
+    //     $('.rule').on('click',(()=>{
+    //         $('.level').show();
+    //         $('.desciption').hide();
+    //         $('.rule').css('border','none');
+    //     }))
+    // }))
     
-
 }
 
-const gamerule = "In Breakout, a layer of bricks lines the top third of the screen and the goal is to destroy them all by repeatedly bouncing a ball off a paddle into them by using 'left' and 'right' key."
-const gamerule2 = "The player has three life. The game is over once the player use up three life. "
-const gameRule = () =>{
-    const $desciption = $('<div>');
-    $('.testPlay').append($desciption);
-    $desciption.addClass('desciption');
-    $desciption.css('width','100px').css('height','300px').css('background-color','white');
-    $desciption.text(gamerule + gamerule2).css('color','black').css('position','absolute').css('font-size','10px');
-    $desciption.css('left',containerWidth/12 +'px').css('bottom', '100px').css('text-align','center');
-    $desciption.hide();
-}
+// const gamerule = "In Breakout, a layer of bricks lines the top third of the screen and the goal is to destroy them all by repeatedly bouncing a ball off a paddle into them by using 'left' and 'right' key."
+// const gamerule2 = "The player has three life. The game is over once the player use up three life. "
+// const gameRule = () =>{
+//     const $desciption = $('<div>');
+//     $('.testPlay').append($desciption);
+//     $desciption.addClass('desciption');
+//     $desciption.css('width','100px').css('height','300px').css('background-color','white');
+//     $desciption.text(gamerule + gamerule2).css('color','black').css('position','absolute').css('font-size','10px');
+//     $desciption.css('left',containerWidth/12 +'px').css('bottom', '100px').css('text-align','center');
+//     $desciption.hide();
+// }
 
 
 const gameLevel1 = () =>{
     const $level1 = $('<button>');
-    $level1.addClass('level1').text('Easy')
+    $level1.addClass('level').text('Easy').attr('onclick','final(6,3,30)');
     $level1.css('width','100px').css('height','50px').css('background-color','white').css('border','1px solid white');
     $level1.css('left',containerWidth/11 +'px').css('bottom', '420px').css('text-align','center').css('position','absolute');
     $('.testPlay').append($level1);
-    $('.level1').on('click',()=>{
-        $('.level1').css('border','3px solid blue')
-    });
-    $('.level1').on('click',()=>{
-        final();
-    });
+    oneClick()
 }
 
 
 const gameLevel2 = () =>{
     const $level2 = $('<button>');
-    $level2.addClass('level2').text('Medium')
+    $level2.addClass('level').text('Medium').attr('onclick','final(6,4,20)');
     $level2.css('width','100px').css('height','50px').css('background-color','white').css('border','1px solid white');
     $level2.css('left',containerWidth/11 +'px').css('bottom', '350px').css('text-align','center').css('position','absolute');
     $('.testPlay').append($level2);
-    $('.level2').on('click',()=>{
-        $('.level2').css('border','3px solid blue')
-    });
-    $('.level2').on('click',()=>{
-        final2();
-    });
+    oneClick()
 }
 
 const gameLevel3 = () =>{
     const $level3 = $('<button>');
-    $level3.addClass('level3').text('Hard')
+    $level3.addClass('level').text('Hard').attr('onclick','final(6,5,10)');
     $level3.css('width','100px').css('height','50px').css('background-color','white').css('border','1px solid white');
     $level3.css('left',containerWidth/11 +'px').css('bottom', '280px').css('text-align','center').css('position','absolute');
     $('.testPlay').append($level3);
-    $('.level3').on('click',()=>{
-        $('.level3').css('border','3px solid blue')
-    });
-    $('.level3').on('click',()=>{
-        final3();
-    });
+    oneClick()
 }
 
-
-
-const final = () =>{
-    score();
-    threeLife();
-    block();   
+let timerId
+let timerId1
+const final = (num1,num2,speed1) =>{
+    // $('.block').remove();
+    $('div.block').remove();
+    $('div.ball').remove();
+    $('div.Player').remove();
+    //location.reload()
+    block(num1,num2);
+    timerId = setInterval(ballMove,speed1)
+    speed = speed1; 
+    clearInterval(timerId); 
+    $('.startButton').on('click',(() =>{
+        timerId1 = setInterval(ballMove,speed);
+        //console.log(speed)
+    }))
     player();
     ball();
     playerMove(); 
-}
-
-const final2 = () =>{
-    blockColumn = 6;
-    blockRow = 4;
-    timerId = setInterval(ballMove,10);
-    score();
-    threeLife();
-    block();   
-    player();
-    ball();
-    playerMove(); 
-}
-
-const final3 = () =>{
-    blockColumn = 6;
-    blockRow = 5;
-    timerId = setInterval(ballMove,5);
-    score();
-    threeLife();
-    block();   
-    player();
-    ball();
-    playerMove(); 
+    // console.log(currentBall[0], currentBall[1])
 }
 
 
+
+const oneClick = () =>{
+    $('.level').click(function(e) {
+    $('.active').css('border','none');
+    $('.level').removeClass('active');
+    $(this).addClass('active');
+    $('.active').css('border','3px solid blue');
+});
+}
+
+
+const newGame = () =>{
+    gameLevel1()
+    gameLevel2()
+    gameLevel3()
+    
+}
 
 $(()=>{
-    //final();
     test();
-    gameLevel1();
-    gameLevel2();
-    gameLevel3();
-    
-    
+    newGame();
 })
